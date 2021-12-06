42134e0c6b5ef017d35d72c1c3509c2a035a9388
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2021-12-06 12:45:13 +0100

    Create R_code_quantitative_estimates_land_cover.r

---
+++ R_code_quantitative_estimates_land_cover.r
@@ -0,0 +1,17 @@
+# R_code_quantitative_estimates_land_cover.r
+
+# first install new packages
+install.packages("ggplot2") # for new graphical properties
+install.packages("gridExtra") # for new multiframe properties
+install.packages("ncdf4") # for managing Copernicus data
+install.packages("RStoolbox")
+
+library(raster)
+# setting the working directory 
+setwd("/Users/matildecervellieri/lab/")
+
+# brick 
+
+
+
+
