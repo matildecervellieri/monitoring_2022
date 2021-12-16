7564cbc05db752c7ebb38a5b04d7637cd12d4fc9
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2021-12-16 11:18:51 +0100

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,12 @@
+# R code for uploading and visualizing Coprnicus data in R
+
+# install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+library(RStoolbox)
+library(ggplot2)
+library(gridExtra)
+# install.packages("viridis")
+library(viridis)
+
+# Set the working directory
