5192d3e9c4d8f1bb8ff27689f22fa0b7899948f5
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2021-12-16 15:09:03 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -9,3 +9,14 @@ library(gridExtra)
 library(viridis)
 
 # Set the working directory
+setwd("/Users/matildecervellieri/lab/copernicus/")
+
+# we upload data and we use the raster function 
+snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
+snow20211214 # snow cover extent (sce)
+
+# plot of this data 
+plot(snow20211214) # higher values in the northern part in green 
+
+
+
