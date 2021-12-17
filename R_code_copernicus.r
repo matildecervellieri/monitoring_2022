7ad29521ddcc9672650200d5d0b3c1e19e0f3628
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2021-12-17 10:25:18 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -7,6 +7,7 @@ library(ggplot2)
 library(gridExtra)
 # install.packages("viridis")
 library(viridis)
+library(patchwork)
 
 # Set the working directory
 setwd("/Users/matildecervellieri/lab/copernicus/")
@@ -39,5 +40,59 @@ ggplot() + geom_raster(snow20211214, mapping = aes(x=x, y=y, fill= Snow.Cover.Ex
 ggplot() + geom_raster(snow20211214, mapping = aes(x=x, y=y, fill= Snow.Cover.Extent)) + scale_fill_viridis(option=“cividis”) 
 
 # we can put names
-ggplot() + geom_raster(snow20211214, mapping = aes(x=x, y=y, fill= Snow.Cover.Extent)) + scale_fill_viridis(option="cividis") + ggtitle("cividis palette”)
+ggplot() + geom_raster(snow20211214, mapping = aes(x=x, y=y, fill= Snow.Cover.Extent)) + scale_fill_viridis(option="cividis") + ggtitle("cividis palette")
+
+# ------ day 2
+# We did this for the image of the 14th of December, now we're dealing with the one of the 29th of August 
+# importing all the data together with the lapply function. The common pattern is SCE
+rlist <- list.files(pattern="SCE")
+rlist
+
+list_rast <- lapply(rlist, raster)
+list_rast # we can see the two files
+
+# now we need to stack all the files together 
+snowstack <- stack(list_rast)
+snowstack
+
+ssummer <- snowstack$Snow.Cover.Extent.1 # summer 
+swinter <- snowstack$Snow.Cover.Extent.2 # winter 
+ssummer
+swinter
+
+# let's make a ggplot 
+ggplot() + geom_raster(ssummer, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) + scale_fill_viridis(option="viridis") + ggtitle("Snow cover during my birthday!")
+# it's a good colour palette because the yellow is for the maximum value 
+
+ggplot() + geom_raster(swinter, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) + scale_fill_viridis(option="viridis") + ggtitle("Snow cover during the 14th of December!")
+# the gray part is like that because data is missing 
+
+# let's patchwork them together
+p1 <- ggplot() + geom_raster(ssummer, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) + scale_fill_viridis(option="viridis") + ggtitle("Snow cover during my birthday!")
+p2 <- ggplot() + geom_raster(swinter, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) + scale_fill_viridis(option="viridis") + ggtitle("Snow cover during the 14th of December!")
+
+p1 / p2
+
+# we can see coordinates, such as latitude and longitude
+# we can zoom to a specific point 
+
+# we can crop your image on a certain area
+
+# longitude from 0 to 20
+# latitude from 30 to 50
+# crop the stack to the extent of Sicily
+ext <- c(0, 20, 30, 50)
+# stack_cropped <- crop(snowstack, ext) # this will crop the whole srack, and 
+ssummer_cropped <- crop(ssummer, ext)
+swinter_cropped <- crop(swinter, ext)
+
+plot(ssummer_cropped)
+plot(swinter_cropped)
+
+p1 <- ggplot() + geom_raster(ssummer_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) + scale_fill_viridis(option="viridis") + ggtitle("Snow cover during my birthday!")
+p2 <- ggplot() + geom_raster(swinter_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) + scale_fill_viridis(option="viridis") + ggtitle("Snow cover during the 14th of December!")
+
+p1 / p2
+
+
 
