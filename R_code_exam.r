cab461ed83b5429c7b88d353dc596bf8f3aace3f
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2022-01-20 15:49:07 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -25,4 +25,14 @@ names(lst) <- c("lst_2010", "lst_2015", "lst_2020")
 names(lst)
 lst
 
+# Let's associate it to an object
+lst2010 <- lst$lst_2010
+lst2015 <- lst$lst_2015
+lst2020 <- lst$lst_2020
 
+cl <- colorRampPalette(c("blue", "light blue", "pink", "yellow"))(100)
+plot(lst2010, col=cl) 
+
+ext <- c(40, 60, 30, 50)
+lst2010_cropped <- crop(lst2010, ext)
+plot(lst2010_cropped, col=cl) 
