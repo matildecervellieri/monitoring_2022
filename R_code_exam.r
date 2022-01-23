5f4578b75a3c30d659630bc3f0f3380462698021
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2022-01-23 17:47:46 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -9,7 +9,9 @@ library(gridExtra)
 # Set the working directory
 setwd("/Users/matildecervellieri/lab/exam/")
 
-# Prediction of the alpine glacier retreat using albedo as an indicator of glacier presence 
+# Prediction of the alpine glacier retreat using albedo as an indicator of glacier presence
+# 3 albedo measurements were used for the years 1999, 2009 and 2009
+# In particular, the detections were made in the period August-September: high albedo values indicate permanent snow and ice, thus avoiding the snow cover of the winter months 
 # importing dark-sky albedo files: first we make a list and then use the lapply function 
 albedo_list <- list.files(pattern="ALBH")
 albedo_list
@@ -37,10 +39,15 @@ albedo2009 <- albedo_cropped$albedo_2009
 albedo2019 <- albedo_cropped$albedo_2019
 
 # let's plot them using cividis palette
-a1 <- ggplot() + geom_raster(albedo1999, mapping = aes(x=x, y=y, fill= albedo_1999)) + scale_fill_viridis(option="cividis") + ggtitle("Glaciers in 1999")
-a2 <- ggplot() + geom_raster(albedo2009, mapping = aes(x=x, y=y, fill= albedo_2009)) + scale_fill_viridis(option="cividis") + ggtitle("Glaciers in 2009")
-a3 <- ggplot() + geom_raster(albedo2019, mapping = aes(x=x, y=y, fill= albedo_2019)) + scale_fill_viridis(option="cividis") + ggtitle("Glaciers in 2019")
+a1 <- ggplot() + geom_raster(albedo1999, mapping = aes(x=x, y=y, fill= albedo_1999)) + scale_fill_viridis(option="cividis") + ggtitle("Alps's glaciers in 1999")
+a2 <- ggplot() + geom_raster(albedo2009, mapping = aes(x=x, y=y, fill= albedo_2009)) + scale_fill_viridis(option="cividis") + ggtitle("Alps's glaciers in 2009")
+a3 <- ggplot() + geom_raster(albedo2019, mapping = aes(x=x, y=y, fill= albedo_2019)) + scale_fill_viridis(option="cividis") + ggtitle("Alps's glaciers in 2019")
 
-# plotting them together to see the changes in the amount of ice over the years: 1999 vs 2009 vs 2019
+# plotting them together to see the changes in the amount of ice and snow over the years: 1999 vs 2009 vs 2019
 a1 / a2 / a3
+
+# Let's save the results 
+
+# ---------- part 2: envestigate in a deeper way the changes in the amount of ice and snow using SCE (snow cover extent)
+
 
