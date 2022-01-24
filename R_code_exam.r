2d6750516fa91d5ccb6add3341cf18fcf6f671f4
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2022-01-24 21:07:57 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -67,7 +67,7 @@ fcover <- stack(fcover_import)
 fcover
 
 # cropping the images: Italy
-ext_italy <- c(3, 20, 35, 49)
+ext_italy <- c(6, 19.5, 36.5, 47)
 fcover_cropped <- crop(fcover, ext_italy)
 fcover_cropped
 
@@ -87,6 +87,106 @@ plot(fcover1999)
 plot(fcover2009)
 plot(fcover2019)
 dev.off()
+
+# let's make a quantative analysis 
+# unsuperClass function: unsupervised classification. We don't explain to the software which is the forest and which the agricultural areas
+
+####### 1999 #######
+fcover1999c <- unsuperClass(fcover1999, nClasses=2) # unsuperClass(x, nClasses)
+fcover1999c 
+
+plot(fcover1999c$map)
+# value 1 = forest
+# value 2 = agricultural areas and water
+
+# now I want to know the percentage of forest and the percentage of agricultural areas
+# the function freq is doing the job, it calculates the number of pixel of forest and agricultural areas
+freq(fcover1999c$map) 
+# forest (class 1) = 305665
+# agricultural areas and water (class 2) = 35627
+
+# let's make the proportion
+total1999 <- 471887 + 332321
+propagri1999 <- 332321 / total1999
+propforest1999 <- 471887 / total1999
+propagri1999
+propforest1999
+
+# build a dataframe
+cover <- c("Forest", "Agriculture") # we use quotes because is text  
+# prop1992 <- c(0.8956114, 0.1043886)
+prop1999 <- c(propforest1999, propagri1999)
+
+proportion1999 <- data.frame(cover, prop1999)
+proportion1999 # this are the real proportion of forest and agriculture: quantitative values 
+
+# we are gonna use the ggplot function. We want to use histograms (geom_bar function with the statistic values, so the actual ones. Fill is the colour inside the bar)
+ggplot(proportion1999, aes(x=cover, y=prop1999, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1)
+
+####### 2009 #######
+fcover2009c <- unsuperClass(fcover2009, nClasses=2) # unsuperClass(x, nClasses)
+fcover2009c 
+
+plot(fcover2009c$map)
+# value 1 = forest
+# value 2 = agricultural areas and water
+
+# now I want to know the percentage of forest and the percentage of agricultural areas
+# the function freq is doing the job, it calculates the number of pixel of forest and agricultural areas
+freq(fcover2009c$map) 
+# forest (class 1) = 305665
+# agricultural areas and water (class 2) = 35627
+
+# let's make the proportion
+total2009 <- 507388 + 296820
+propagri2009 <- 296820 / total2009
+propforest2009 <- 507388 / total2009
+propagri2009
+propforest2009
+
+# build a dataframe
+cover <- c("Forest", "Agriculture") # we use quotes because is text  
+# prop1992 <- c(0.8956114, 0.1043886)
+prop2009 <- c(propforest2009, propagri2009)
+
+proportion2009 <- data.frame(cover, prop2009)
+proportion2009 # this are the real proportion of forest and agriculture: quantitative values 
+
+# we are gonna use the ggplot function. We want to use histograms (geom_bar function with the statistic values, so the actual ones. Fill is the colour inside the bar)
+ggplot(proportion2009, aes(x=cover, y=prop2009, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1)
+
+
+####### 2019 #######
+fcover2019c <- unsuperClass(fcover2019, nClasses=2) # unsuperClass(x, nClasses)
+fcover2019c 
+
+plot(fcover2019c$map)
+# value 1 = forest
+# value 2 = agricultural areas and water
+
+# now I want to know the percentage of forest and the percentage of agricultural areas
+# the function freq is doing the job, it calculates the number of pixel of forest and agricultural areas
+freq(fcover2019c$map) 
+# forest (class 1) = 305665
+# agricultural areas and water (class 2) = 35627
+
+# let's make the proportion
+total2019 <- 437644 + 366564
+propagri2019 <- 366564 / total2019
+propforest2019 <- 437644 / total2019
+propagri2019
+propforest2019
+
+# build a dataframe
+cover <- c("Forest", "Agriculture") # we use quotes because is text  
+# prop1992 <- c(0.8956114, 0.1043886)
+prop2019 <- c(propforest2019, propagri2019)
+
+proportion2019 <- data.frame(cover, prop2019)
+proportion2019 # this are the real proportion of forest and agriculture: quantitative values 
+
+# we are gonna use the ggplot function. We want to use histograms (geom_bar function with the statistic values, so the actual ones. Fill is the colour inside the bar)
+ggplot(proportion2019, aes(x=cover, y=prop2019, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1)
 
 
 # part 3: 
