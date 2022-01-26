b6a65898188bfab82ee4da5131a41b3a3658abfa
matildecervellieri <92572790+matildecervellieri@users.noreply.github.com>
2022-01-26 12:55:26 +0100

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,4 +1,4 @@
-# Ecological and environmental changes during the years in Italy: glaciers, forests and temperatures
+# Ecological and environmental changes during the years in Italy: glaciers, forests and thropic state of lakes
 library(raster)
 library(ggplot2)
 library(RStoolbox)
@@ -10,8 +10,8 @@ library(gridExtra)
 # Set the working directory
 setwd("/Users/matildecervellieri/lab/exam/")
 
-# Prediction of the alpine glacier retreat using albedo as an indicator of glacier presence
-# 3 albedo measurements were used for the years 1999, 2009 and 2009
+# Prediction of the Alpine glacier retreat using albedo as an indicator of glacier presence
+# 3 albedo measurements were used for the years 1999, 2009 and 2009, taken from Copernicus Global Land Service
 # In particular, the detections were made in the period August-September: high albedo values indicate permanent snow and ice, thus avoiding the snow cover of the winter months 
 
 # importing dark-sky albedo files: first we make a list and then use the lapply function 
@@ -48,6 +48,14 @@ a3 <- ggplot() + geom_raster(albedo2019, mapping = aes(x=x, y=y, fill= albedo_20
 # plotting them together to see the changes in the amount of ice and snow over the years: 1999 vs 2009 vs 2019
 a1 / a2 / a3
 
+# difference between 1999 and 2019 ice conditions
+adif <- albedo1999 - albedo2019
+adif
+# plotting the difference beetween 1999 and 2019 ice conditions in the Alps 
+acl <- colorRampPalette(c("navyblue", "royalblue3","white", "red2"))(100)
+plot(adif, col=acl, main="difference between 1999 and 2019 ice conditions in the Alps")
+
+
 # Let's save the results 
 pdf("albedo.pdf")
 grid.arrange(a1, a2, a3, nrow=3)
@@ -58,14 +66,20 @@ png("albedo.png",
 grid.arrange(a1, a2, a3, nrow=3)
 dev.off()
 
+png("_albedo_difference.png", 
+    width = 1500, height = 2000)
+plot(adif, col=acl, main="difference between 1999 and 2019 ice conditions in the Alps")
+dev.off()
 
-# ---------- part 2: envestigate in a deeper way the recent changes in the amount of ice and snow using SCE (snow cover extent)
-# yearly changes from 2017 to 2021: always using detections made in the summer season (Sept. 2)
 
+# ---------- part 2: changes over the years in the Italian forest area using the Forest Cover data from Copernicus Global Land Service
+# ten years changes: 1999 vs 2009 vs 2019
+
+# importing the FCOVER data
 fcover_list <- list.files(pattern="FCOVER")
 fcover_list
 
-fcover_import <- lapply(fcover_list, brick, varname="FCOVER") # same as doing fcover_import <- lapply(fcover_list, raster) because the first layer is FCOVER
+fcover_import <- lapply(fcover_list, raster) # same as doing fcover_import <- lapply(fcover_list, brick, varname="FCOVER") because the first layer is FCOVER
 fcover_import # 3 files imported inside r
 
 # stacking them all together
@@ -81,6 +95,7 @@ fcover1999 <- fcover_cropped$Fraction.of.green.Vegetation.Cover.1km.1
 fcover2009 <- fcover_cropped$Fraction.of.green.Vegetation.Cover.1km.2
 fcover2019 <- fcover_cropped$Fraction.of.green.Vegetation.Cover.1km.3
 
+# First step: qualitative analysis 
 par(mfrow=c(1,3))
 plot(fcover1999, main="Forest cover 1999")
 plot(fcover2009, main="Forest cover 2009")
@@ -94,7 +109,7 @@ plot(fcover2009, main="Forest cover 2009")
 plot(fcover2019, main="Forest cover 2019")
 dev.off()
 
-# let's make a quantative analysis 
+# Second step: let's make a quantative analysis in order to estimate the percentage of forest loss in 20 years (1999 vs 2019)
 # unsuperClass function: unsupervised classification. We don't explain to the software which is the forest and which the agricultural areas
 
 ####### 1999 #######
@@ -115,8 +130,8 @@ freq(fcover1999c$map)
 total1999 <- 471887 + 332321
 propagri1999 <- 332321 / total1999
 propforest1999 <- 471887 / total1999
-propagri1999
-propforest1999
+propagri1999 # 41% of agricoltural and water areas
+propforest1999 # 59% of forest
 
 # build a dataframe
 cover <- c("Forest", "Agriculture") # we use quotes because is text  
@@ -125,7 +140,7 @@ prop1999 <- c(propforest1999, propagri1999)
 proportion1999 <- data.frame(cover, prop1999)
 proportion1999 # this are the real proportion of forest and agriculture: quantitative values 
 
-# we are gonna use the ggplot function. We want to use histograms
+# we are gonna use the ggplot function: histograms
 ggplot(proportion1999, aes(x=cover, y=prop1999, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1) + ggtitle("Proportion of 1999")
 
 
@@ -146,8 +161,8 @@ freq(fcover2019c$map)
 total2019 <- 437644 + 366564
 propagri2019 <- 366564 / total2019
 propforest2019 <- 437644 / total2019
-propagri2019
-propforest2019
+propagri2019 # 46% of agricoltural and water areas
+propforest2019 # 54% of forest: -5% of forests
 
 # build a dataframe
 cover <- c("Forest", "Agriculture") # we use quotes because is text  
@@ -162,7 +177,8 @@ ggplot(proportion2019, aes(x=cover, y=prop2019, color=cover)) + geom_bar(stat="i
 # plotting the differences between 1999 conditions and 2019 contitions
 fdif <- fcover1999c$map - fcover2019c$map
 cl <- colorRampPalette(c("tan3", "yellow", "forest green"))(100)
-plot(fdif, col=cl) # yellow parts are the area in which forest has been lost, while green indicates the continuos presence of forest and brown the unchanged agricultural zones
+plot(fdif, col=cl, main="Difference between 1999 and 2019") # in this case: yellow parts are the area in which forest has been lost, while green indicates the continuos presence of forest and brown the unchanged agricultural zones
+# if the unsuperClass function gives different values to the forest and agricultural areas, the colour palette should be adjusted in order to point out changes
 
 # plotting them all together
 pdf("quantitative forest difference map.pdf")
@@ -174,13 +190,15 @@ dev.off()
 
 g1 <- ggplot(proportion1999, aes(x=cover, y=prop1999, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1) + ggtitle("Proportion of 1999")
 g2 <- ggplot(proportion2019, aes(x=cover, y=prop2019, color=cover)) + geom_bar(stat="identity", fill="white") + ylim(0,1) + ggtitle("Proportion of 2019")
+g1 + g2 # loss of 5% of forests
 
 pdf("quantitative forest difference graph.pdf")
 g1 + g2
 dev.off()
 
 
-# part 3: 
+# ------------ part 3: changes during the years in the thropic state index (layer of Lake Water Quality dataset from Copernicus Global Land Service) of Northern Italy lakes 
+# Comparison between the lakes trophic conditions of the years 2004, 2008, 2012, 2016, 2020
 swi_list <- list.files(pattern="SWI")
 swi_list
 
